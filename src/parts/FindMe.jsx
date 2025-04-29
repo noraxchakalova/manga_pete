2d0de2439efe177f749cda4f03e59c24dcaeb202
nchakalova <email@nora_chakalova@abv.bg>
2025-04-29 22:05:29 +0300

    fix embedded map

--- src/parts/FindMe.jsx
+++ src/parts/FindMe.jsx
@@ -31,7 +31,7 @@ const FindMe = () => {
         <div className="mt-8">
           <iframe
             title="Google Maps Location"
-            src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.161000508127!2d27.989617176351757!3d43.227083971125474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4566051f8a26f%3A0xd6bcbf4e6cdeed0f!2sTrakataPrimorski%2C%20ul.%20%2213-ta%22!5e0!3m2!1sen!2sbg!4v1745953174718!5m2!1sen!2sbg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.161000508127!2d27.989617176351757!3d43.227083971125474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4566051f8a26f%3A0xd6bcbf4e6cdeed0f!2sTrakataPrimorski%2C%20ul.%20%2213-ta%22!5e0!3m2!1sen!2sbg!4v1745953174718!5m2!1sen!2sbg"
             width="100%"
             height="450"
             allowFullScreen=""
